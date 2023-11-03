7870420f55b70df4fe11308d9c03d6e6795b93b5
Zlatko <iliev.zlatko@gmail.com>
2023-11-03 19:49:55 +0200

    initial commit

---
+++ apps/auth/src/auth.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UsersModule } from './users/users.module';
+import { LoggerModule } from '@app/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  imports: [
+    UsersModule,
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+    JwtModule.registerAsync({
+      useFactory: (configSevice: ConfigService) => ({
+        secret: configSevice.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configSevice.get<number>('JWT_EXPIRATION')}s`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
